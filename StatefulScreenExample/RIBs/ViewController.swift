df42289575c1100e1fa59df8a018bd905c8cc5f2
Dmitriy Ignatyev <diignatev@beeline.ru>
2019-12-05 23:26:48 +0300

    init

---
+++ StatefulScreenExample/RIBs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StatefulScreenExample
+//
+//  Created by Dmitriy Ignatyev on 05.12.2019.
+//  Copyright © 2019 IgnatyevProd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
